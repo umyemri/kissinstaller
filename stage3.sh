326a8182af9907004898cc72567505b00f802aaa
Andrew Sikes <40582475+umyemri@users.noreply.github.com>
2020-11-20 19:17:30 -0800

    Update stage3.sh
    sigh... kernel not kernal...

--- stage3.sh
+++ stage3.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# stage 3: prepping for the kernal
+# stage 3: prepping for the kernel
 #
-# most of this is getting basic application in place for creation of the kernal
+# most of this is getting basic application in place for creation of the kernel
 #
 
 echo -n "hostname: "
@@ -32,6 +32,6 @@ wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.9.9.tar.xz
 tar xvf linux-5.9.9.tar.xz
 cd linux-*
 
-echo 'ready for kernal generation'
+echo 'ready for kernel generation'
 echo 'follow the usual process - i\'ll make a version of this for my xps 9360'
-echo 'but that\'s going to be just for me. you're mileage may very.'
+echo 'but that\'s going to be just for me. you\'re mileage may very.'
