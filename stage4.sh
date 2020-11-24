28ce8699d460a44ca8be47e14d497c7c023d41a5
Andrew Sikes <40582475+umyemri@users.noreply.github.com>
2020-11-24 14:02:18 -0800

    Update stage4.sh

--- stage4.sh
+++ stage4.sh
@@ -6,13 +6,13 @@
 # the config from the arch installer.
 #
 
-echo -n "version number of kernel: "
+echo -n "version number of kernel (ex. 5.9.2): "
 read linux_ver
 
 # move to where the kernel package and .config is
 cd ~/linux-${linux_ver}
 
-make -j "$(nproc)" 
+make -j "$(nproc)" #long process here
 make install
 
 mv /boot/vmlinuz    /boot/vmlinuz-${linux_ver}
