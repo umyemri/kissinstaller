1887526866c41993ee56c6490dfca4d0bc2de5fe
Andrew Sikes <40582475+umyemri@users.noreply.github.com>
2020-11-24 13:24:17 -0800

    Update stage4.sh

--- stage4.sh
+++ stage4.sh
@@ -2,6 +2,18 @@
 #
 # stage 4: kernel generation
 #
-#
+# method that i used was the localyesconfig but there is ways of just copying
+# the config from the arch installer.
 #
 
+echo -n "version number of kernel: "
+read linux_ver
+
+# move to where the kernel package and .config is
+cd ~/linux-${linux_ver}
+
+make -j "$(nproc)" 
+make install
+
+mv /boot/vmlinuz    /boot/vmlinuz-${linux_ver}
+mv /boot/System.map /boot/System.map-${linux_ver}    
