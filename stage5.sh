0e45378aa6dace052db1aea20afc4701a126e1e4
Andrew Sikes <40582475+umyemri@users.noreply.github.com>
2020-11-24 13:43:25 -0800

    Create stage5.sh

---
+++ stage5.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# stage 5: boot loader
+#
+# 
+#
+
+# used later for efi schema
+#echo -n "which uefi (y/n): "
+#read uefi
+
+kiss b grub && kiss i grub 
+kiss b efibootmgr && kiss i efibootmgr
+
+# bios only
+#grub-install /dev/sda
+#grub-mkconfig -o /boot/grub/grub.cfg
+
+# uefi only
+grub-install --target=x86_64-efi \
+             --efi-directory=/boot \
+             --bootloader-id=kiss
+grub-mkconfig -o /boot/grub/grub.cfg
+
+kiss b baseinit && kiss i baseinit 
+
+echo "install complete! be sure to add a user and change root's password."
+echo "root password: passwd"
+echo "adding a user: adduser username"
