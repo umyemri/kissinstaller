fffde7f96bc39cbcb24552fc4a390fd1f27766d3
Andrew Sikes <40582475+umyemri@users.noreply.github.com>
2020-12-16 20:25:35 -0800

    Create arch_prep.sh

---
+++ arch_prep.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+
+curl -sL https://git.io/JLcLe > stage1.sh
+curl -sL https://git.io/Jk2Hf > stage2.sh
+curl -sL https://git.io/Jk2bf > stage3.sh
+curl -sL https://git.io/JLc4L > stage4.sh
+curl -sL https://git.io/JLc44 > stage5.sh
+curl -sL https://git.io/JLcEN > config
+pacman -Sy wget
